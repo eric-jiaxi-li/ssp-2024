d6fb1829837a3c6af272c6cca15d8c34c1982eab
ericli0480 <ericjli0480@gmail.com>
2024-07-14 23:20:59 -0600

    fixed LiOD4

--- odlib.py
+++ odlib.py
@@ -681,7 +681,88 @@ def diff_photometry(fits_file, ref_stars_txt, ast_x, ast_y):
 
 
 
+def gen_eph(fits_file, sun_vec, earth_tilt_deg, eph_Y, eph_M, eph_D, obs_Y, obs_M, obs_D):
+    """
+    Params: Text file containing the orbital elements (Note:
+            may vary from time to time) at observation time
+            Earth-to-sun vector as a np array in equatorial coords
+            Tilt of earth in degrees, 23.4384987711 deg
+            Date of desired ephemeris: Y, M, D at 0 UTC
+            Observation time in same format
+            All units AU, days
+    Return: RA/DEC of asteroid at the specified time
+    """
+    eph_time_julian = julian(eph_Y, eph_M, eph_D)
+    obs_time_julian = julian(obs_Y, obs_M, obs_D)
+    interval_Gaussian = days_to_GD(eph_time_julian - obs_time_julian)
+    
+    # Each value on a separate line
+    fin = open(fits_file)
+    EC = float(fin.readline()) # Eccentricity
+    QR = float(fin.readline())
+    IN = float(fin.readline()) # Inclination (deg)
+    OM = float(fin.readline()) # Long of asc node (deg)
+    W = float(fin.readline()) # Arg of perifocus (deg)
+    Tp = float(fin.readline())
+    N = float(fin.readline())
+    MA = float(fin.readline()) # Mean anomaly (deg)
+    TA = float(fin.readline())
+    A = float(fin.readline()) # Semimajor axis (AU)
+    AD = float(fin.readline())
+    PR = float(fin.readline())
+    
+	# Rename variables, convert all to rad
+    a = A
+    e = EC
+    i = radians(IN)
+    omega = radians(OM)
+    w = radians(W)
+    M = radians(MA)
 
+    # Update mean anomaly
+    period_Gaussian = sqrt(4 * (pi ** 2) * (a ** 3))
+    M += 2 * pi * interval_Gaussian / period_Gaussian 
+    M = (M % (2 * pi)) 
+
+    if debug == True:
+        print("Interval in GD", interval_Gaussian)
+        print("Period in GD", period_Gaussian)
+        print(radians(MA))
+
+    E = solve_kepler(M, e, threshold = 1e-9) # Eccentric anomaly, rad
+
+    
+    r = np.array([a * cos(E) - a * e, a * sqrt(1 - e ** 2) * sin(E), 0])
+    
+    omega_spin = np.array([[cos(omega), -sin(omega), 0], 
+                           [sin(omega), cos(omega), 0], 
+                           [0, 0, 1]])
+    i_spin = np.array([[1, 0, 0], 
+                       [0, cos(i), -sin(i)], 
+                       [0, sin(i), cos(i)]])
+    w_spin = np.array([[cos(w), -sin(w), 0], 
+                       [sin(w), cos(w), 0], 
+                       [0, 0, 1]])
+    r_ec = omega_spin @ i_spin @ w_spin @ r
+
+    # Correct for tilt of earth
+    tilt = radians(earth_tilt_deg) # Angle of earth's axis
+    tilt_spin = np.array([[1, 0, 0], 
+                          [0, cos(tilt), -sin(tilt)], 
+                          [0, sin(tilt), cos(tilt)]])
+    r_eq = tilt_spin @ r_ec # r in equatorial coordinates
+
+    # Range vector
+    rho = r_eq + sun_vec
+    rho_hat = rho / mag(rho)
+
+    # RA and DEC
+    DEC = degrees(asin(rho_hat[2]))
+    sin_RA = (rho_hat[1] / cos(radians(DEC)))
+    cos_RA = (rho_hat[0] / cos(radians(DEC)))
+    RA = quadrant_deg(sin_RA, cos_RA)
+    
+    return RA, DEC
 
 
 
