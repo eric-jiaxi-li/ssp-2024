7b6d0c20c431d2a9bd221a0d2cfa02d2e6f95625
ericli0480 <ericjli0480@gmail.com>
2024-07-22 16:23:22 -0600

    Switched dimensions of message to make image clearer

--- misc_code/arecibo/arecibo.py
+++ misc_code/arecibo/arecibo.py
@@ -9,17 +9,16 @@ import matplotlib.pyplot as plt
 fin = open("misc_code/arecibo/arecibo.txt")
 message = fin.readline()
 message_num = int(message)
+print(int(str(message_num), 2))
 message = np.float64(np.array(list(message)))
 n = len(message) # 1679
 
 
-# Determine dimensions of image (1679 is semiprime) -> 23 73
+# Determine dimensions of image (1679 is semiprime) -> 23 73 or 73 23
 for x in range(1,n):
     if 1679 % x == 0:
         print("Possible dimensions: ", x, n // x)
-message = np.reshape(message, (23, 73))
-# plt.imshow(message)
-# plt.show()
-
-
-print(int(str(message_num), 2))
+message = np.reshape(message, (73, 23))
+plt.imshow(message)
+plt.gray()
+plt.show()
