557fe4e3798ced3e1bdd1e9b9f4420a44287877e
ericli0480 <ericjli0480@gmail.com>
2024-07-24 23:12:17 -0600

    Finished visualization of asteroid orbit

--- visualize.py
+++ visualize.py
@@ -8,15 +8,15 @@ from math import *
 import numpy as np
 import odlib
 
-debug = True
+debug = False
 
 # Orbital elements from LiOD.py
 a = 3.245761552052847 # Checked
 e = 0.6141866747698732 # Checked
 i = radians(9.98211483642601) # Checked
-omega = radians(148.00823478480334)
+omega = radians(148.00823478480334) # Checked
 w = radians(142.38377885236548) # Checked (145.532--close enough?)
-m = radians(359.16509165962776)
+m = radians(359.16509165962776) # Don't need to check
 
 E = odlib.solve_kepler(m, e, threshold = 1e-9) # Eccentric anomaly, rad
 r = np.array([a * cos(E) - a * e, a * sqrt(1 - e ** 2) * sin(E), 0])
@@ -51,6 +51,7 @@ if debug == True:
     aphelion = -1
     perehelion = 1e9
     i_debug = -1
+    vern_equinox_vec = np.array([1, 0, 0]) # Vernal equinox = x-axis
     asc_node_vec = np.array([0, 0, 0])
     perehelion_vec = np.array([0, 0, 0])
     w_debug = -1
@@ -89,15 +90,25 @@ while True:
     if debug == True:
         aphelion = max(aphelion, mag(asteroid_pos))
         perehelion = min(perehelion, mag(asteroid_pos))
+
         i_debug = max(i_debug, abs(degrees(atan(asteroid_pos.z / np.sqrt(asteroid_pos.x ** 2 + asteroid_pos.y ** 2)))))
+
         if abs(asteroid_pos.z) < 1e-3:
             asc_node_vec = np.array([asteroid_pos.x, asteroid_pos.y, asteroid_pos.z])
         if (mag(asteroid_pos) - perehelion) < 1e-3:
             perehelion_vec = np.array([asteroid_pos.x, asteroid_pos.y, asteroid_pos.z])
+        omega_debug = degrees(acos(np.dot(asc_node_vec, vern_equinox_vec) / (odlib.mag(asc_node_vec) * odlib.mag(vern_equinox_vec))))
+
         w_debug = degrees(acos(np.dot(asc_node_vec, perehelion_vec) / (odlib.mag(asc_node_vec) * odlib.mag(perehelion_vec))))
 
-        label(pos = vector(1000, 1000, 0), text = "a = " + str((aphelion + perehelion) / 2))
-        label(pos = vector(1000, 1000, 300), text = "e = " + str((aphelion - perehelion) / (aphelion + perehelion)))
-        label(pos = vector(1000, 1000, 600), text = "i = " + str(i_debug))
-        label(pos = vector(1000, 1000, 900), text = "w = " + str(w_debug))
+
+
+        """
+        Comment the below lines if they lag too much!
+        """
+        # label(pos = vector(1000, 1000, 0), text = "a = " + str((aphelion + perehelion) / 2))
+        # label(pos = vector(1000, 1000, 300), text = "e = " + str((aphelion - perehelion) / (aphelion + perehelion)))
+        # label(pos = vector(1000, 1000, 600), text = "i = " + str(i_debug))
+        # label(pos = vector(1000, 1000, 900), text = "omega = " + str(omega_debug))
+        # label(pos = vector(1000, 1000, 1200), text = "w = " + str(w_debug))
 
