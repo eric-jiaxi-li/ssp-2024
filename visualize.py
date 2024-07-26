0d30c17b54706f2dee3bea93373ea293d5ed6148
ericli0480 <ericjli0480@gmail.com>
2024-07-25 19:24:37 -0600

    Moved visualization data to text file

--- visualize.py
+++ visualize.py
@@ -14,19 +14,28 @@ debug = False
 # Julian day of these elements: 2460502.000185
 # Generate new planets with Osculating Orbital Elements (center at Sun) at this Julian day
 # A, EC, IN, OM, W, MA
-# Current bodies: asteroid, Earth
-a_list = np.array([3.245761552052847, 1.000525628690563E+00])
-e_list = np.array([0.6141866747698732, 1.621348364416402E-02])
-i_list = np.array([9.98211483642601, 2.343670108483834E+01])
-omega_list = np.array([148.00823478480334, 3.599966754616262E+02])
-w_list = np.array([142.38377885236548, 1.019166684209871E+02])
-m_list = np.array([359.16509165962776, 1.866838072819970E+02])
-col_list = np.array([color.white, color.blue])
+# Current bodies: asteroid, Mercury, Venus, Earth
+
+fin = open("inputs/2012FN62/visualize.txt")
+fin.readline() # Body names
+r_list = np.float64(np.array(fin.readline().split()))
+a_list = np.float64(np.array(fin.readline().split()))
+e_list = np.float64(np.array(fin.readline().split()))
+i_list = np.float64(np.array(fin.readline().split()))
+omega_list = np.float64(np.array(fin.readline().split()))
+w_list = np.float64(np.array(fin.readline().split()))
+m_list = np.float64(np.array(fin.readline().split()))
+col_list = np.array([color.white, color.gray(0.5), vector(1,0.7,0.2), color.green, color.red, vector(1,0.7,0.2), vector(0.8, 0.6, 0.6), color.cyan, color.blue])
 
 odlib.visualize_multiple_orbits(a_list, e_list, i_list, omega_list, w_list, m_list, col_list)
 
 
-
+# a_list = np.array([3.245761552052847, 3.870970735948419E-01, 7.233275855465966E-01, 1.000525628690563E+00, 1.523659834502866E+00, 5.202599117666169E+00, 9.562130184723967E+00, 1.930353687033573E+01, 3.021761915800814E+01])
+# e_list = np.array([0.6141866747698732, 2.056492674549958E-01, 6.731253872798876E-03, 1.621348364416402E-02, 9.329437184231582E-02, 4.827540669964238E-02, 5.492259660375094E-02, 4.504514062884678E-02, 1.335627148974016E-02])
+# i_list = np.array([9.98211483642601, 2.855338780923179E+01, 2.443666664720525E+01, 2.343670108483834E+01, 2.467738264916708E+01, 2.323459959325644E+01, 2.255391444000841E+01, 2.366318869414800E+01, 2.229201912670373E+01])
+# omega_list = np.array([148.00823478480334, 1.098000323307333E+01, 8.003564838735068E+00, 3.599966754616262E+02, 3.366286755647931E+00, 3.249840197664308E+00, 5.949276474098442E+00, 1.851424116208393E+00, 3.481735881232009E+00])
+# w_list = np.array([142.38377885236548, 6.761040833562714E+01, 1.244937007456587E+02, 1.019166684209871E+02, 3.331080758672448E+02, 1.112999441927799E+01, 8.458621110447142E+01, 1.628595797087438E+02, 3.283428388177517E+01])
+# m_list = np.array([359.16509165962776, 1.098334645764942E+02, 4.519739861556728E-01, 1.866838072819970E+02, 3.303246732997781E+01, 4.450086296331610E+01, 2.598097561227970E+02, 2.537407994715349E+02, 3.226387684813254E+02])
 
 
 
